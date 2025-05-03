34b949b86df2e3ccceac755310239ce438ea9a2f
Mohamed Yasser Abdel Fattah <Mohamed.Yasser@ibm.com>
2025-05-03 17:59:33 +0300

    Reports Done

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,7 +2,7 @@ const Wallet = require('../models/wallet')
 const Order = require('../models/order')
 const Client = require('../models/client')
 const Iron = require('../models/iron')
-
+const { isDateBetween } = require('../controllers/irons')
 const subtractOneMonth = (dateStr) => {
     let date = new Date(dateStr);
 
@@ -39,21 +39,6 @@ const formatDate = (date) => {
     return `${month}/${day}/${year}, ${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
 }
 
-const isBeforeByMonthYear = (date1, date2) => {
-    let d1 = new Date(date1);
-    let d2 = new Date(date2);
-
-    let year1 = d1.getFullYear();
-    let month1 = d1.getMonth() + 1; 
-
-    let year2 = d2.getFullYear();
-    let month2 = d2.getMonth() + 1;
-
-    if (year2 < year1 || (year2 === year1 && month2 < month1)) {
-        return true;  
-    }
-    return false;
-}
 
 const isBeforeByMonthYearOrEqual = (date1, date2) => {
     let d1 = new Date(date1);
@@ -80,10 +65,100 @@ const isSameMonth = (date1Str, date2Str) => {
     );
 }
 
+const getBeginningOfMonthIronPrice = async(startDate) => {
+    console.log(startDate)
+    let ironList, ironMap = {};
+    try {
+        ironList = await Iron.find()
+        for(let i of ironList){
+            if(!(i.name in ironMap)){
+                ironMap[i.name] = {}
+            }
+            for(let j of i["costPerWeight"]){
+                if(i["radius"] in ironMap[i.name]){
+                    let obj = {}
+                    obj= {"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}
+                    ironMap[i.name][`${i["radius"]}`].push(obj)
+                    
+                } 
+                else{
+                    ironMap[i.name][`${i["radius"]}`] =[{"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}]
+                }
+            }
+        }
+        const orders = await Order.find({
+            state: { $in: ["جاري انتظار الدفع", "منتهي"] }
+        });
+        const wantedDate = new Date(startDate);
+        for(let order of orders){
+            let orderDate = new Date(order.date)
+            if(isDateBetween(orderDate,wantedDate)){
+                for(let ticket of order.ticket){
+                    for(let consumedUnitPerWeight of ticket.usedUnitCostPerWeight){
+                        let hasTheConsumedIron = false
+                        for(let k = 0 ; k<ironMap[ticket.ironName][ticket.radius].length ; k++){
+                            if( consumedUnitPerWeight.orderId === ironMap[ticket.ironName][ticket.radius][k]._id){
+                                hasTheConsumedIron = true    
+                                if(order.type === "out"){
+                                    ironMap[ticket.ironName][ticket.radius][k].weight += consumedUnitPerWeight.weight
+                                }
+                                else{
+                                    ironMap[ticket.ironName][ticket.radius][k].weight -= consumedUnitPerWeight.weight
+                                }
+                            }                            
+                        }
+                        if(!hasTheConsumedIron){
+                            ironMap[ticket.ironName][ticket.radius].push({"weight":consumedUnitPerWeight.weight,"unitCostPerTon":consumedUnitPerWeight.cost})
+                        }
+                    }
+                }
+            }
+        }
+        let total = 0
+        for (const ironName in ironMap) {
+            const radii = ironMap[ironName];
+            for (const radius in radii) {
+              const items = radii[radius];
+              for (const item of items) {
+                if(item.weight>=0)
+                    total+= parseFloat(item.weight/1000) * item.unitCost 
+              }
+            }
+        }
+        console.log(total)
+        return total
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+function convertToLastDayOfMonth(dateString) {
+    // Parse the input date string.
+    const date = new Date(dateString);
+  
+    // Check if the date is valid.
+    if (isNaN(date.getTime())) {
+      return null; // Return null for invalid date input.
+    }
+  
+    // Get the year and month.
+    const year = date.getFullYear();
+    const month = date.getMonth(); // Month is 0-indexed.
+  
+    // Create a new date object for the last day of the month.
+    // Passing '0' as the day to the Date constructor results in the last day of the *previous* month.
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+  
+    // Convert the last day of the month back to ISO 8601 string format.
+    const lastDayOfMonthString = lastDayOfMonth.toISOString();
+    return lastDayOfMonthString;
+  }
+
 const getProfitReportDataBasedOnDate = async(req,res)=>{
     let { monthAndYear } = req.body
     console.log(monthAndYear)
-    let soldOrders, iron,soldProfit = 0, purchasedPrice = 0, beginningOfMonthIronPrice = 0, endingOfMonthIronPrice = 0, totalProfitWithoutExpenses = 0, overAllTotalProfit = 0;
+    let soldOrders, iron,soldProfit = 0, purchasedPrice = 0, beginningOfMonthIronPrice = 0, endingOfMonthIronPrice = 0, totalProfitWithoutExpenses = 0, overAllTotalProfit = 0, deliveryFees = 0;
     let retObj;
     try{
         iron = await Iron.find()
@@ -98,10 +173,12 @@ const getProfitReportDataBasedOnDate = async(req,res)=>{
         console.log(soldOrders.length)
 
         for(let order of soldOrders){
-            // console.log(order.date, monthAndYear)
-            if(order.date === monthAndYear ){
+            console.log(order.date, monthAndYear)
+            if(isSameMonth(order.date ,monthAndYear )){
+                console.log("sold prof", order.realTotalPrice, order.type)
                 if(order.type === "out"){
-                    soldProfit += order.totalProfit
+                    soldProfit += order.realTotalPrice
+                    deliveryFees += order.deliveryFees
                 }
                 if(order.type === "in"){
                     purchasedPrice+= order.realTotalPrice
@@ -110,33 +187,27 @@ const getProfitReportDataBasedOnDate = async(req,res)=>{
         }
 
         let formattedSentMonthAndYear = monthAndYear
-        // console.log(formattedSentMonthAndYear)
+        let monthBefore = subtractOneMonth(formattedSentMonthAndYear)
         for(let i of iron){
             for(let j of i.costPerWeight){
-                let monthBefore = subtractOneMonth(formattedSentMonthAndYear)
-                // console.log(isBeforeByMonthYear(monthAndYear, j.date))
                 if(isBeforeByMonthYearOrEqual(monthAndYear, j.date)){
                     endingOfMonthIronPrice += (parseFloat((j.weight / 1000)) * j.unitCostPerTon)
                 }
-                console.log(isBeforeByMonthYearOrEqual(monthBefore, j.date),monthBefore, j.date)
-                if(isBeforeByMonthYearOrEqual(monthBefore, j.date) ){
-                    // console.log("heeree")
-                    beginningOfMonthIronPrice += (parseFloat((j.weight / 1000)) * j.unitCostPerTon)
-                }
             }
         }
+        let formattedDay = convertToLastDayOfMonth(monthBefore)
+        beginningOfMonthIronPrice = await getBeginningOfMonthIronPrice(formattedDay)
 
         let companyExpensesDoc = await Client.findOne({"clientId":"4"})
-        // console.log(companyExpensesDoc["purchasingNotes"])
         let companyExpenses = 0
         for(let i of companyExpensesDoc["purchasingNotes"]){
             if(isSameMonth(i.date,monthAndYear)){
                 companyExpenses+=i.amount
             }
         }
-
-        totalProfitWithoutExpenses = (soldProfit + endingOfMonthIronPrice) - (purchasedPrice + beginningOfMonthIronPrice) 
-        overAllTotalProfit = totalProfitWithoutExpenses - companyExpenses
+        console.log(deliveryFees,"fees")
+        totalProfitWithoutExpenses = ((soldProfit + endingOfMonthIronPrice) - (purchasedPrice + beginningOfMonthIronPrice)) + deliveryFees
+        overAllTotalProfit = (totalProfitWithoutExpenses - companyExpenses) 
 
         let totalDeficitAndSurplusOfGoods = 0
         retObj = {
--- backend/controllers/irons.js
+++ backend/controllers/irons.js
@@ -46,115 +46,27 @@ const addIron = async (req, res) => {
     res.json(newIron)
 }
 
-const addIronWeight = (req, res) => {
 
-}
 
-const subtractIronWeight = (req, res) => {
-
-}
-
-function isBeforeToday(dateString) {
-    const givenDate = new Date(dateString);
-    const today = new Date();
-  
-    // Remove time from both dates (set to midnight)
-    givenDate.setHours(0, 0, 0, 0);
-    today.setHours(0, 0, 0, 0);
-  
-    return givenDate < today;
-}
-
-const getIronStorageOwner = async(req, res) => {
-    let { startDate } = req.body
-    console.log(startDate)
-    let ironList, ironMap = {};
-    try {
-        ironList = await Iron.find()
-        for(let i of ironList){
-            totalWeight = 0
-            for(j of i["costPerWeight"]){
-                totalWeight += j["weight"]
-            }
-            if(i.name in ironMap){
-                ironMap[i.name].push({"radius":i["radius"],"weight":totalWeight})
-            }
-            else{
-                ironMap[i.name] = [
-                    {"radius":i["radius"],"weight":totalWeight}
-                ]
-            }
-        }
-        Object.keys(ironMap).forEach(key => {
-            ironMap[key].sort((a, b) => Number(a.radius) - Number(b.radius));
-        });
-        let ironArray = Object.entries(ironMap).map(([key, value]) => ({ [key]: value }));
-        const orders = await Order.find({
-            state: { $in: ["جاري انتظار الدفع", "منتهي"] }
-        });
-        const wantedDate = new Date(startDate);
-        for(let order of orders){
-            let orderDate = new Date(order.date)
-            if(wantedDate>=orderDate && isBeforeToday(wantedDate)){
-                for(let ticket of order.ticket){
-                    for(let k = 0 ; k<ironArray.length ; k++){
-                        if(ticket.ironName in ironArray[k]){
-                            for(let l = 0 ; l<ironArray[k][ticket.ironName].length ; l ++){
-                                if(ironArray[k][ticket.ironName][l]["radius"] == ticket.radius  ){
-                                    if(ticket.ironName === "عز" && ticket.radius == '10'){
-                                        console.log(ticket.ironName)
-                                        console.log(ironArray[k][ticket.ironName][l]["radius"])
-                                        console.log(ticket.netWeight)
-                                        console.log(order.type)
-                                    }    
-                                    if(order.type === "out"){
-                                        ironArray[k][ticket.ironName][l]["weight"] += ticket.netWeight
-                                    }
-                                    else{
-                                        ironArray[k][ticket.ironName][l]["weight"] -= ticket.netWeight
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        res.json(ironArray)
-    }
-    catch(err){
-        console.log(err)
-    }
-}
-
-function sortRadiusKeys(data) {
-    const sortedData = {};
-  
-    for (const brand in data) {
-      const items = data[brand];
-  
-      if (Array.isArray(items) && items.length > 0) {
-        const sortedItems = items
-          .map(obj => {
-            const key = Object.keys(obj)[0];
-            return { key: parseInt(key), original: obj };
-          })
-          .sort((a, b) => a.key - b.key)
-          .map(item => item.original);
-  
-        sortedData[brand] = sortedItems;
-      } else {
-        sortedData[brand] = items;
-      }
-    }
-  
-    return sortedData;
+const resetTime = (date) => {
+    date.setHours(0, 0, 0, 0);
+    return date;
   }
+  
+const isDateBetween = (target, end) => {
+    const targetDate = resetTime(new Date(target));
+    let tod = new Date().toLocaleString('en-EG', { timeZone: 'Africa/Cairo' })
+    const startDate = resetTime(new Date(tod))
+    const endDate = resetTime(new Date(end));
+    console.log(startDate, endDate, targetDate)
+    return  startDate>= targetDate && endDate < targetDate;
+}
+  
+  
   
 
 const getIronStorageAdmin = async(req, res) => {
     let { startDate } = req.body
-    console.log(startDate)
     let ironList, ironMap = {};
     try {
         ironList = await Iron.find()
@@ -163,16 +75,14 @@ const getIronStorageAdmin = async(req, res) => {
                 ironMap[i.name] = {}
             }
             for(let j of i["costPerWeight"]){
-                if(j["weight"]>0){
-                    if(i["radius"] in ironMap[i.name]){
-                        let obj = {}
-                        obj= {"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}
-                        ironMap[i.name][`${i["radius"]}`].push(obj)
-                      
-                    } 
-                    else{
-                        ironMap[i.name][`${i["radius"]}`] =[{"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}]
-                    }
+                if(i["radius"] in ironMap[i.name]){
+                    let obj = {}
+                    obj= {"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}
+                    ironMap[i.name][`${i["radius"]}`].push(obj)
+                    
+                } 
+                else{
+                    ironMap[i.name][`${i["radius"]}`] =[{"weight":j["weight"],"unitCost":j["unitCostPerTon"],"id": j["_id"]}]
                 }
             }
         }
@@ -182,23 +92,23 @@ const getIronStorageAdmin = async(req, res) => {
         const wantedDate = new Date(startDate);
         for(let order of orders){
             let orderDate = new Date(order.date)
-            if(wantedDate>=orderDate && isBeforeToday(wantedDate)){
+            if(isDateBetween(orderDate,wantedDate)){
                 for(let ticket of order.ticket){
                     for(let consumedUnitPerWeight of ticket.usedUnitCostPerWeight){
                         let hasTheConsumedIron = false
-                        for(let k = 0 ; k<ironArray[ticket.ironName][ticket.radius].length ; k++){
-                            if( consumedUnitPerWeight.orderId === ironArray[ticket.ironName][ticket.radius][k]._id){
+                        for(let k = 0 ; k<ironMap[ticket.ironName][ticket.radius].length ; k++){
+                            if( consumedUnitPerWeight.orderId === ironMap[ticket.ironName][ticket.radius][k]._id){
                                 hasTheConsumedIron = true    
                                 if(order.type === "out"){
-                                    ironArray[ticket.ironName][ticket.radius][k] += consumedUnitPerWeight.weight
+                                    ironMap[ticket.ironName][ticket.radius][k].weight += consumedUnitPerWeight.weight
                                 }
                                 else{
-                                    ironArray[ticket.ironName][ticket.radius][k] -= consumedUnitPerWeight.weight
+                                    ironMap[ticket.ironName][ticket.radius][k].weight -= consumedUnitPerWeight.weight
                                 }
                             }                            
                         }
                         if(!hasTheConsumedIron){
-                            ironArray[ticket.ironName][ticket.radius].push({"weight":consumedUnitPerWeight.weight,"unitCostPerTon":consumedUnitPerWeight.cost})
+                            ironMap[ticket.ironName][ticket.radius].push({"weight":consumedUnitPerWeight.weight,"unitCostPerTon":consumedUnitPerWeight.cost})
                         }
                     }
                 }
@@ -210,13 +120,17 @@ const getIronStorageAdmin = async(req, res) => {
             for (const radius in radii) {
               const items = radii[radius];
               for (const item of items) {
-                total+=item.weight
-                let rowitem = {
-                  name: ironName,
-                  weight: item.weight,
-                  radius: radius,
-                };
-                ironStorage.push(rowitem);
+                if(item.weight>=0){
+                    total+=item.weight
+                    let rowitem = {
+                    name: ironName,
+                    weight: item.weight,
+                    radius: radius,
+                    price: item.unitCost,
+                    totalPrice: parseFloat(item.weight/1000) * item.unitCost,
+                    };
+                    ironStorage.push(rowitem);
+                }
               }
             }
           }
@@ -227,69 +141,8 @@ const getIronStorageAdmin = async(req, res) => {
     }
 }
 
-const getIronStorage = async(req, res) => {
-    let { startDate } = req.body
-    console.log(startDate)
-    let ironList, ironMap = {};
-    try {
-        ironList = await Iron.find()
-        for(let i of ironList){
-            totalWeight = 0
-            for(j of i["costPerWeight"]){
-                totalWeight += j["weight"]
-            }
-            if(i.name in ironMap){
-                ironMap[i.name].push({"radius":i["radius"],"weight":totalWeight})
-            }
-            else{
-                ironMap[i.name] = [
-                    {"radius":i["radius"],"weight":totalWeight}
-                ]
-            }
-        }
-        Object.keys(ironMap).forEach(key => {
-            ironMap[key].sort((a, b) => Number(a.radius) - Number(b.radius));
-        });
-        let ironArray = Object.entries(ironMap).map(([key, value]) => ({ [key]: value }));
-        const orders = await Order.find({
-            state: { $in: ["جاري انتظار الدفع", "منتهي"] }
-        });
-        const wantedDate = new Date(startDate);
-        for(let order of orders){
-            let orderDate = new Date(order.date)
-            if(wantedDate>=orderDate && isBeforeToday(wantedDate)){
-                for(let ticket of order.ticket){
-                    for(let k = 0 ; k<ironArray.length ; k++){
-                        if(ticket.ironName in ironArray[k]){
-                            for(let l = 0 ; l<ironArray[k][ticket.ironName].length ; l ++){
-                                if(ironArray[k][ticket.ironName][l]["radius"] == ticket.radius  ){
-                                    if(ticket.ironName === "عز" && ticket.radius == '10'){
-                                        console.log(ticket.ironName)
-                                        console.log(ironArray[k][ticket.ironName][l]["radius"])
-                                        console.log(ticket.netWeight)
-                                        console.log(order.type)
-                                    }    
-                                    if(order.type === "out"){
-                                        ironArray[k][ticket.ironName][l]["weight"] += ticket.netWeight
-                                    }
-                                    else{
-                                        ironArray[k][ticket.ironName][l]["weight"] -= ticket.netWeight
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        res.json(ironArray)
-    }
-    catch(err){
-        console.log(err)
-    }
-}
 
-let x = 1000;
+let x = 3950;
 const getScaleWeight = (req, res) => {
     res.json({ "weight": x })
     // x+= 1000
@@ -380,12 +233,10 @@ const handleChangePassword = (req, res) => {
 }
 
 module.exports = {
-    getIronStorage,
     addIron,
-    addIronWeight,
-    subtractIronWeight,
     changeIronWeight,
     getScaleWeight,
     handleChangePassword,
-    getIronStorageAdmin
+    getIronStorageAdmin,
+    isDateBetween
 }
